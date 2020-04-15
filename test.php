07b764848571ab0835d43b0f68b3c64f34a07eda
Mohammad Atwi <mega.web.own@gmail.com>
2020-04-15 20:24:19 +0300

    Update test.php

--- test.php
+++ test.php
@@ -7,17 +7,17 @@ $dbtool = new dbtool;
 $db = $dbtool->connect('localhost', 'root', 'my_password', 'test_db');
 
 /* insert */
-//$db->insert("into my_table (title) values (:title)", ['title' -> 'Hello world!']); // return true
-//$db->insert("into my_table (title) values (:title)", ['title' -> 'Hello my world!'], true); // return new record id
+//$db->insert("into my_table (title) values (:title)", ['title' => 'Hello world!']); // return true
+//$db->insert("into my_table (title) values (:title)", ['title' => 'Hello my world!'], true); // return new record id
 
 /* update */
-//$db->update("my_table set title = :title", ['title' -> 'Hello new world!']); // return true
+//$db->update("my_table set title = :title", ['title' => 'Hello new world!']); // return true
 
 /* delete */
-//$db->delete("from my_table where id = :id", ['id' -> 57]); // return true
+//$db->delete("from my_table where id = :id", ['id' => 57]); // return true
 
 /* select */
-//$data = $db->select("id, title, about from my_table where status = :status", ['status' -> 2]); // return data array
+//$data = $db->select("id, title, about from my_table where status = :status", ['status' => 2]); // return data array
 /*
 foreach($data as $row){
 echo $row['title'] . '<br>';
